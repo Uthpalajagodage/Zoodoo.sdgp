d9e720ffca8a0a02bdb25faa52023db387c6873d
Uthpalajagodage <101706471+Uthpalajagodage@users.noreply.github.com>
2023-06-29 01:34:02 +0530

    form.js

---
+++ ZOODO-master/zoodoo/client/src/pages/Form.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./Form.css"
+import { useState } from 'react'
+
+const Form = () => {
+    const [data, setData]=useState({name:"",email:"",phone:"", message:""});
+    const handleChange =(e) =>{
+        const name = e.target.name;
+        const value = e.target.value;
+        setData({...data, [name]:value})
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        console.log(data);
+    }
+  return (
+    <form method='post' onSubmit={handleSubmit}>
+        <h1>Contact <span>Us</span></h1>
+        <input type="text" name="name" id="" onChange={handleChange} value={data.name} placeholder='Enter Name'/>
+        <input type="email" name="email" id="" onChange={handleChange} value={data.email} placeholder='example@gmail.com' />
+        <input type="phone" name="phone" id="" onChange={handleChange} value={data.phone} placeholder='+94'/>
+        <textarea name="message" id="" onChange={handleChange} value={data.message} cols="30" rows="10" placeholder='type here...'/>
+        <button type='submit'>send</button>
+        <p>{data.name},{data.email},{data.phone},{data.message}</p>    
+
+    </form>
+  )
+}
+
+export default Form
